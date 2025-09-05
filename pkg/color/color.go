652963ecba874e4a60007ae1237d700024c658be
Dark Yagami <cm0614633@gmail.com>
2025-09-05 14:07:16 +0530

    Update color.go

--- pkg/color/color.go
+++ pkg/color/color.go
@@ -1,1 +1,12 @@
 
+package color
+
+const (
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Red     = "\033[31m"
+	Cyan    = "\033[36m"
+	Magenta = "\033[35m"
+	Reset   = "\033[0m"
+)
