0b89233d62cd6194c045abd2e6b07d5ed2c0ef2a
Dark Yagami <cm0614633@gmail.com>
2025-09-05 14:06:51 +0530

    Update flags.go

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,1 +1,29 @@
 
+package config
+
+import "flag"
+
+// All configuration options
+type Config struct {
+	Help          bool
+	GitHubNoreply bool
+	Popular       bool
+	CustomDomain  bool
+	OutputFile    string
+	Quiet         bool
+}
+
+// ParseFlags parses command line flags and returns a Config
+func ParseFlags() *Config {
+	cfg := &Config{}
+
+	flag.BoolVar(&cfg.Help, "h", false, "Show help menu")
+	flag.BoolVar(&cfg.GitHubNoreply, "g", false, "Show only GitHub noreply emails")
+	flag.BoolVar(&cfg.Popular, "p", false, "Show only popular email domains")
+	flag.BoolVar(&cfg.CustomDomain, "cd", false, "Show only custom domains")
+	flag.StringVar(&cfg.OutputFile, "o", "", "Save output to file (.csv or .txt)")
+	flag.BoolVar(&cfg.Quiet, "q", false, "Quiet mode: no info or progress output")
+
+	flag.Parse()
+	return cfg
+}
