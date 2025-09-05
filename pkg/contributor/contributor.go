79b71e457995108e15986aec639d47e13f1f6745
Dark Yagami <cm0614633@gmail.com>
2025-09-05 14:08:11 +0530

    Update contributor.go

--- pkg/contributor/contributor.go
+++ pkg/contributor/contributor.go
@@ -1,1 +1,22 @@
 
+package contributor
+
+import (
+	"sort"
+	"strings"
+	"sync"
+)
+
+// Contributor represents a git contributor
+type Contributor struct {
+	Name  string
+	Email string
+	mu    sync.Mutex
+}
+
+// SortByName sorts contributors alphabetically by name
+func SortByName(contributors []Contributor) {
+	sort.Slice(contributors, func(i, j int) bool {
+		return strings.ToLower(contributors[i].Name) < strings.ToLower(contributors[j].Name)
+	})
+}
